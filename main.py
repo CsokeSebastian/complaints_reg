e7a64238a7455c714cb7b6fbab28bd48ded0bbeb
CsokeSebastian <csokesebastian@yahoo.ro>
2023-03-25 23:42:51 +0100

    small changes to the variables

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from authentication import add_admin, must_log_in, log_out, IS_ADMIN_LOGGED_IN
+from authentication import add_admin, must_log_in, log_out, is_admin_logged_in
 from table_function import *
 from text_messages import *
 
 
-EXIT = 0
-ADD_COMPLAINT = 1
-LOG_IN = 2
-LOG_OUT = 3
-ADD_ADMIN = 4
-VIEW_ALL = 5
-VIEW_UNRESOLVED = 6
-CHANGE_STATUS_COMPLAINT = 7
-# DELETE_COMPLAINT = 8
-IS_ADMIN_LOGGED_IN = False
+exit_app = 0
+add_complaint = 1
+login = 2
+logout = 3
+add_new_admin = 4
+view_all = 5
+view_resolved = 6
+change_status_complaint = 7
+is_admin_logged_in = False
 
 
 while True:
@@ -21,45 +20,42 @@ while True:
         IntroMessages.chose_admin_or_user_message()
         IntroMessages.option_chose_message()
         option = int(input("Please select the corresponding number:\n"))
-        if option == LOG_IN:
+        if option == login:
             AdminMessages.must_log_in_message()
-            IS_ADMIN_LOGGED_IN = must_log_in()
-            if IS_ADMIN_LOGGED_IN == True:
+            is_admin_logged_in = must_log_in()
+            if is_admin_logged_in == True:
                 AdminMessages.log_in_message()
-                while IS_ADMIN_LOGGED_IN == True:
+                while is_admin_logged_in == True:
                     IntroMessages.options_admin_message()
                     option_admin = int(input("What would you like to do?\n"))
-                    if option_admin == ADD_ADMIN:
+                    if option_admin == add_new_admin:
                         AdminMessages.must_enter_credentials_message()
                         add_admin()
                         continue
-                    elif option_admin == VIEW_ALL:
+                    elif option_admin == view_all:
                         ComplaintsMessages.view_all_message()
                         view_all_complaints()
                         continue
-                    elif option_admin == VIEW_UNRESOLVED:
+                    elif option_admin == view_resolved:
                         ComplaintsMessages.unresolved_complaints_message()
                         unresolved_complaints()
                         continue
-                    elif option_admin == CHANGE_STATUS_COMPLAINT:
+                    elif option_admin == change_status_complaint:
                         ComplaintsMessages.mark_as_resolved_message()
                         mark_as_resolved()
                         continue
-                    elif option_admin == LOG_OUT:
+                    elif option_admin == logout:
                         AdminMessages.log_out_message()
                         AdminMessages.goodbye_message()
                         log_out()
                         break
             else:
-                break
-        elif option == ADD_COMPLAINT:
+                continue
+        elif option == add_complaint:
             ComplaintsMessages.add_new_complaint_message()
             add_new_complaint()
             ComplaintsMessages.done_add_new_complaint_message()
-
-        elif option == EXIT:
+        elif option == exit_app:
             exit()
     except ValueError:
         print("Enter a valid option!")
-
-
